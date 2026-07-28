24f10783468004c59de91b5007d5eebd53efd24a
Serhii Yatsyna <yatsyna.sergey@gmail.com>
2026-07-28 18:55:10 +0300

    feat: enhance message passing example with type specification and sleep functionality

--- lessons/practice_on_message_passing_1/src/main.rs
+++ lessons/practice_on_message_passing_1/src/main.rs
@@ -1,13 +1,15 @@
 use std::sync::mpsc;
-use std::thread;
+use std::thread::{self, sleep};
+use std::time::Duration;
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel::<i32>();
     let tx_clone = tx.clone();
 
     thread::spawn(move || {
         let my_vec = vec![1, 2, 3, 4, 5];
         for i in my_vec {
+            // sleep(Duration::from_secs(5));
             tx.send(i).unwrap();
         }
     });
@@ -15,6 +17,7 @@ fn main() {
     thread::spawn(move || {
         let my_vec = vec![6, 7, 8, 9, 10];
         for i in my_vec {
+            // sleep(Duration::from_secs(5));
             tx_clone.send(i).unwrap(); // fix this line
         }
     });
